28d502a98daa0d4b54d41804c3ecbd802e262421
Andrew Kandels <akandels@gmail.com>
2012-01-16 03:43:53 +0000

    style changes;

--- header.php
+++ header.php
@@ -1,77 +1,32 @@
-<?php
-/**
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package WordPress
- * @subpackage wpbootstrap
- * @since wpbootstrap 0.1
- */
-?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 * We filter the output of wp_title() a bit -- see
-	 * twentyten_filter_wp_title() in functions.php.
-	 */
-	wp_title('|', true, 'right');
-	?>
-	</title>
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<!--[if lt IE 9]>
-	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<link href="<?php bloginfo('template_directory'); ?>/js/google-code-prettify/prettify.css" rel="stylesheet">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="<?php bloginfo('template_directory'); ?>/js/google-code-prettify/prettify.css" rel="stylesheet">
 <?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 * Loading JQuery too
-	 */
-	wp_enqueue_script( 'jquery' );
-	if (is_singular() && get_option( 'thread_comments')) {
-		wp_enqueue_script('comment-reply');
-    }
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
+    wp_enqueue_script( 'jquery' );
+    wp_head();
 ?>
 </head>
 <body <?php body_class(); ?>>
 <div class="container">
-    <div class="hero-unit" style="position: relative">
-        <img src="/wp-content/plugins/vulnero/public/images/logo.png" alt=""/>
+    <div class="hero-unit">
+        <a href="/"><img src="/wp-content/plugins/vulnero/public/images/logo.png" alt=""/></a>
         <h1><?php bloginfo( 'name' ); ?></h1>
         <p><?php bloginfo( 'description' ); ?></p>
     </div>
     <div id="access" role="navigation">
-        <?php 
-            global $sa_options;
-            $sa_settings = get_option('sa_options', $sa_options);
-        ?>
-		<?php if ($sa_settings['nav_view'] == 'pills'):
-            wp_nav_menu(array(
-                'container_class' => 'menu-header', 
-                'theme_location'  => 'primary', 
-                'menu_class'      => 'pills', 
-                'fallback_cb'     => false
-            )); 
-        elseif ($sa_settings['nav_view'] == 'tabs'):
-            wp_nav_menu(array(
-                'container_class' => 'menu-header', 
-                'theme_location'  => 'primary',
-                'menu_class'      => 'tabs',
-                'fallback_cb'     => false
-            )); 
-        endif; ?>
+        <?php wp_nav_menu(array(
+            'container_class' => 'menu-header',
+            'theme_location'  => 'primary',
+            'menu_class'      => 'tab-navigation',
+            'fallback_cb'     => false
+        )); ?>
     </div><!-- #access -->
--- loop.php
+++ loop.php
@@ -1,124 +1,32 @@
-<?php
-/**
- * The loop that displays posts.
- *
- * The loop displays the posts and the post content.  See
- * http://codex.wordpress.org/The_Loop to understand it and
- * http://codex.wordpress.org/Template_Tags to understand
- * the tags used in it.
- *
- * This can be overridden in child themes with loop.php or
- * loop-template.php, where 'template' is the loop context
- * requested by a template. For example, loop-index.php would
- * be used if it exists and we ask for the loop with:
- * <code>get_template_part( 'loop', 'index' );</code>
- *
- * @package WordPress
- * @subpackage wpbootstrap
- * @since wpbootstrap 0.1
- */
-?>
 <?php if (!have_posts()) : ?>
-    <div class="page-header">
-        <h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-    </div>
-
+    <div class="page-header"><h1><?php _e( 'Not Found', 'twentyten' ); ?></h1></div>
     <div class="alert-message block-message error">
         <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
-        <div class="alert-actions"><?php get_search_form(); ?></div>
     </div>
 <?php endif; ?>
 
-<?php
-	global $sa_options;
-	$sa_settings = get_option( 'sa_options', $sa_options );
-?>
+<?php if (function_exists('bcn_display')): ?>
+    <?php
+        ob_start();
+        bcn_display_list();
+        $output = str_replace('</a>', '</a> <span class="divider">/</span>', ob_get_contents());
+        ob_end_clean();
+    if (strpos($output, '</a>') !== false): ?>
+        <ul class="breadcrumb"><?php echo $output; ?></ul>
+    <?php endif; ?>
+<?php endif; ?>
 
-<?php
-	/* Start the Loop.
-	 *
-	 * In Twenty Ten we use the same loop in multiple contexts.
-	 * It is broken into three main parts: when we're displaying
-	 * posts that are in the gallery category, when we're displaying
-	 * posts in the asides category, and finally all other posts.
-	 *
-	 * Additionally, we sometimes check for whether we are on an
-	 * archive page, a search page, etc., allowing for small differences
-	 * in the loop on each template without actually duplicating
-	 * the rest of the loop that is shared.
-	 *
-	 * Without further ado, the loop:
-	 */ ?>
-
-<?php
-// Set the first post value to 1, outside the loop
-$compactHomepage = !empty($sa_settings['compact_homepage']);
-if ( ( is_paged() == false && $compactHomepage ) || ( !$compactHomepage ) ) {
-    $firstpost  = '1';
-}
-?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-    <?php if ($firstpost == '1'): ?>
+<?php while (have_posts()): the_post(); ?>
     <article>
         <div class="page-header">
-            <h1>
-                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h1>
+            <h1><?php the_title(); ?></a></h1>
         </div> <!-- /page-header -->
-    <?php endif; ?>
-
-    <?php if ($firstpost == ''): ?>
-        <div class="page-header" style="margin-top: 50px;">
-            <h2>
-                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h2>
-        </div> <!-- /page-header -->
-    <?php endif; ?>
-
-    <?php
-    // Compact home page if start
-        if ( $firstpost == '1' ) :
-    ?>
-
-    <div class="post_content">
-        <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
-                <?php the_excerpt(); ?>
-        <?php else : ?>
-                <?php the_content( __( 'Continue reading &rarr;', 'twentyten' ) ); ?>
-                <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
-        <?php endif; ?>
-    </div>
-
-    <p class="muted">
-        <?php twentyten_posted_on(); ?>
-    </p>
-
+        <div class="post_content">
+            <?php if (is_archive() || is_search()): // Only display excerpts for archives and search. ?>
+                    <?php the_excerpt(); ?>
+            <?php else : ?>
+                    <?php the_content(__('Continue reading &rarr;', 'twentyten')); ?>
+            <?php endif; ?>
+        </div>
 	</article>
-
-	<?php
-	// Block this part of the loop by setting $firstpost to '0'
-		if ($compactHomepage && is_home() == true) {
-            $firstpost = '';
-        }
-	?>
-
-	<?php
-	// Compact home page if end
-	 endif; ?>
-
 <?php endwhile; // End the loop. Whew. ?>
-
-<?php /* Display navigation to next/previous pages when applicable */ ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-	<div class="pagination">
-		<ul>
-			<li class="prev">
-				<?php next_posts_link( __( '&larr; Older posts', 'twentyten' ) ); ?>
-			</li>
-			<li class="next">
-				<?php previous_posts_link( __( 'Newer posts &rarr;', 'twentyten' ) ); ?>
-			</li>
-		</ul>
-	</div>
-<?php endif; ?>
